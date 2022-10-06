d7c744d28a8f01c7e4177b5f697bd24f92fc0d0f
devianggiotaviani <devianggi057@gmail.com>
2022-10-06 12:52:46 +0700

    upgrade tugas

--- faperta/views.py
+++ faperta/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from . models import Dosen, Mahasiswa, Staff
 
 # Create your views here.
 def Faperta(request):
-    return render(request, 'faperta.html')
+    judul = ["Program Studi Agribisnis", "Program Studi Agroekoteknologi", "Program Studi Ilmu Perikanan", "Program Studi Teknologi Pangan"]
+
+    dosen = Dosen.objects.all()
+    staff = Staff.objects.all()
+    mahasiswa = Mahasiswa.objects.all()
+
+    konteks = {
+        'jdl': judul,
+        'dataDosen': dosen,
+        'dataStaff': staff,
+        'dataMahasiswa': mahasiswa,
+    }
+    return render(request,'faperta.html', konteks)
--- fh/views.py
+++ fh/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from . models import Dosen, Mahasiswa, Staff
 
 # Create your views here.
 def Fh(request):
-    return render(request, 'fh.html')
+    judul = ["Ilmu Hukum"]
+
+    dosen = Dosen.objects.all()
+    staff = Staff.objects.all()
+    mahasiswa = Mahasiswa.objects.all()
+
+
+    konteks = {
+        'jdl': judul,
+        'dataDosen': dosen,
+        'dataStaff': staff,
+        'dataMahasiswa': mahasiswa,
+    }
+    return render(request, 'fh.html', konteks)
